c999eaa8603685acbf450ff7c679aacbd7e43acd
shirohashiny <shirohashiny@gmail.com>
2025-08-01 04:20:47 +0900

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="p-4 text-center">
+      <h1 className="text-xl font-bold">トップページ</h1>
+      <p>URLに /deck-viewer を追加してデッキ表示画面へ移動</p>
+    </main>
+  );
+}
